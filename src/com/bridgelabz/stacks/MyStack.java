9e94bf47f97a25aaf7e491f82cdaaa9fa6f0c8a0
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-02 11:29:55 +0530

    Added : peek and pop method to delete and display the top most element.

--- src/com/bridgelabz/stacks/MyStack.java
+++ src/com/bridgelabz/stacks/MyStack.java
@@ -14,6 +14,35 @@ public class MyStack<T> {
         head=newNode;
     }
     /**
+     * Deleting the last inserted value first according to LIFO
+     */
+    public void pop()
+    {
+        if (head==null)
+        {
+            System.out.println("Stack is Empty");
+        }
+        else{
+            Node tempNode = head;
+            head = head.next;
+            System.out.println("Popped element is :"+tempNode.data);
+
+
+            }
+    }
+    /**
+     * printing the top most element in the Stack
+     */
+    public void peek(){
+        if(head==null){
+            System.out.println("Stack is Empty");
+        }
+        else
+        {
+            System.out.println("The top most Element is " + head.data);
+        }
+    }
+    /**
      * printing the elements in the stack.
      */
     public void show(){
--- src/com/bridgelabz/stacks/Runner.java
+++ src/com/bridgelabz/stacks/Runner.java
@@ -11,5 +11,12 @@ public class Runner {
             myStack.push(30);
             myStack.push(56);
             myStack.show();
+            MyStack <Integer>myStack1 = new MyStack<Integer>();
+            myStack1.push(70);
+            myStack1.push(30);
+            myStack1.push(56);
+            myStack1.pop();
+            myStack1.show();
+            myStack1.peek();
         }
 }
